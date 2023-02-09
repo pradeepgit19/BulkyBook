1f245f5c567dca325d02b9df38442eedfa2c2641
pradeepkp19 <119552679+pradeepgit19@users.noreply.github.com>
2023-02-09 08:03:21 -0500

    added shopping cart model to db

--- BulkyBook.Modelds/ShoppingCart.cs
+++ BulkyBook.Modelds/ShoppingCart.cs
@@ -1,12 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BulkyBook.Models
 {
     public class ShoppingCart
     {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ValidateNever]
+        [ForeignKey("ProductId")]
         public Product Product { get; set; }
 
         [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [ValidateNever]
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
